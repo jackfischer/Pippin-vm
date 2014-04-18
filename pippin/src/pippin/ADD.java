7dae24c6a59c78b9c5ef667cd72b2747ee6e6971
sbravo1 <sbravo1@binghamton.edu>
2014-04-18 19:02:03 -0400

    Fixed ADD

--- pippin/src/pippin/ADD.java
+++ pippin/src/pippin/ADD.java
@@ -1,5 +1,22 @@
 package pippin;
 
-public class ADD {
+public class ADD extends Instruction {
 
+    public ADD(Machine machine, Memory memory) {
+        super(machine, memory);
+    }
+
+    @Override
+    public int execute(int arg, boolean indirect)
+            throws DataAccessException {
+        int retVal = getMachine().getAccumulator();
+        if (indirect) {
+            int arg1 = getMemory().getData(arg);
+            retVal += getMemory().getData(arg1);            
+        } else {
+            retVal += getMemory().getData(arg);         
+        }
+        getMachine().incrementCounter();
+        return retVal;      
+    }
 }
