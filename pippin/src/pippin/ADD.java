7b60058760a93a74189ceb2a1f5950f018516600
dromero3 <dromero3@binghamton.edu>
2014-05-03 18:34:30 -0400

    should compile with Machine

--- pippin/src/pippin/ADD.java
+++ pippin/src/pippin/ADD.java
@@ -5,7 +5,7 @@ package pippin;
  */
 public class ADD extends Instruction {
 
-    public ADD(Machine machine, Memory memory) {
+    public ADD(Machine machine, MemoryInterface memory) {
         super(machine, memory);
     }
 
@@ -19,10 +19,10 @@ public class ADD extends Instruction {
             throws DataAccessException {
         int retVal = getMachine().getAccumulator();
         if (indirect) {
-            retVal += getMemory().getData(getMemory().getData(arg));            
+            retVal += getMemoryInterface().getData(getMemoryInterface().getData(arg));            
         }
         else {
-            retVal += getMemory().getData(arg);         
+            retVal += getMemoryInterface().getData(arg);         
         }
         getMachine().incrementCounter();
         return retVal;      
