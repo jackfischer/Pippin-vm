d1203c9a37114c6a9608bfb9bfdcfab2be11f607
dromero3 <dromero3@binghamton.edu>
2014-05-03 18:46:46 -0400

    same

--- pippin/src/pippin/MemoryInterface.java
+++ pippin/src/pippin/MemoryInterface.java
@@ -1,22 +1,18 @@
 package pippin;
 
-public interface MemoryInterface {
+import pippin.CodeAccessException;
+import pippin.DataAccessException;
 
-	int CODE_SIZE = 0;
-	int DATA_SIZE = 0;
+public interface MemoryInterface { 
+    static final int DATA_SIZE = 512; 
+	static final int CODE_SIZE = 256; 
 
-	void setCode(int i, int j);
+	void setData(int index, int value) throws DataAccessException; 
+	int getData(int index) throws DataAccessException; 
+	void setCode(int index, long value) throws CodeAccessException; 
+	long getCode(int index) throws CodeAccessException; 
+	void clearCode(); 
+	void clearData(); 
 
-	void setCode(int index, long lng) throws CodeAccessException;
-
-	void setData(int index, int value) throws DataAccessException;
-
-	int getData(int index) throws DataAccessException;
-
-	long getCode(int index) throws CodeAccessException;
-
-	void clearCode();
-
-	void clearData();
 
 }
