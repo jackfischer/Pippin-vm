9e08148fc9f4f4db11ad563c6410400174307bd7
dromero3 <dromero3@binghamton.edu>
2014-05-03 18:43:24 -0400

    same

--- pippin/src/pippin/JUMP.java
+++ pippin/src/pippin/JUMP.java
@@ -5,7 +5,7 @@ package pippin;
  * 
  */
 public class JUMP extends Instruction{
-	public JUMP(Machine machine, Memory memory){
+	public JUMP(Machine machine, MemoryInterface memory){
 		super(machine, memory);
 	}
 	/**
@@ -17,7 +17,7 @@ public class JUMP extends Instruction{
 	public int execute(int arg, boolean indirect) throws DataAccessException{
 		int retVal = getMachine().getAccumulator();
 		if(indirect){
-			getMachine().setProgramCounter(getMemory().getData(arg));
+			getMachine().setProgramCounter(getMemoryInterface().getData(arg));
 		}
 		else{
 			getMachine().setProgramCounter(arg);
