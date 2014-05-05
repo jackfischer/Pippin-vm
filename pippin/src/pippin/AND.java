e7cfec13f8131aba6169dba8b989093270cfa77b
sbravo1 <sbravo1@binghamton.edu>
2014-05-05 11:18:59 -0400

    AND had been changed to CMPZ for some reason. Changed it back.

--- pippin/src/pippin/AND.java
+++ pippin/src/pippin/AND.java
@@ -1,27 +1,33 @@
 package pippin;
 /**
- * COMPARE ZERO instruction: opcode 13
- * @author Jack
+ * AND instruction: opcode 10
+ * @author Lander
  *
  */
-public class CMPZ extends Instruction{
-    public CMPZ (Machine machine, MemoryInterface memory){
-		super(machine, memory);
-	}
-	
-	/**
-	 * @param arg type X: memory address
-	 * if the data value stored at data memory location X is 0, return 1, 
-	 * else return 0 (ignore indirect), 
-	 * increment the program counter.
-	 */
-	@Override
-	public int execute(int arg, boolean indirect) throws DataAccessException {
-		int retVal = 0;
-		if (getMemoryInterface().getData(arg) == 0){
-			retVal = 1;
-		}
-		getMachine().incrementCounter();
-		return retVal;
-	}
+public class AND extends Instruction {
+
+    public AND(Machine machine, Memory memory) {
+        super(machine, memory);
+    }
+    
+    /**
+     * @param arg type X: memory address
+     * if the contents of the accumulator and the data value stored at data memory location X are both non-zero, return 1,
+     * else return 0 (ignore indirect),
+     * increment the program counter.
+     */
+    @Override
+    public int execute(int arg, boolean indirect)
+            throws DataAccessException {
+        int retVal = getMachine().getAccumulator();
+        int operand = getMemory().getData(arg);
+        if(retVal != 0 && operand != 0) {
+            retVal = 1;
+        }
+        else {
+            retVal = 0;
+        }
+        getMachine().incrementCounter();
+        return retVal;      
+    }
 }
