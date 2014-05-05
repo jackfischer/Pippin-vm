dd09895d926eb2cf519eb55be9fdb39f6119c337
dromero3 <dromero3@binghamton.edu>
2014-05-05 19:49:27 -0400

    Corrected implementation

--- pippin/src/pippin/STO.java
+++ pippin/src/pippin/STO.java
@@ -1,27 +1,18 @@
 package pippin;
-/**
- * STORE instruction, opcode 03
- * @author Sam
- */
+
 public class STO extends Instruction{
-	public STO(Machine machine, MemoryInterface memory){
+    public STO(Machine machine, MemoryInterface memory){
 		super(machine, memory);
 	}
 	
-	/**
-	 * @param arg type X: memory address
-	 * (indirect false) store the contents of the accumulator in data memory location X.
-	 * (indirect true) use the data value stored at data memory location X as the location of where to store the contents of the accumulator
-	 * increment the program counter
-	 */
 	@Override
 	public int execute(int arg, boolean indirect) throws DataAccessException{
 		int retVal = getMachine().getAccumulator();
 		if(indirect){
-			getMemoryInterface().setData(retVal, getMemoryInterface().getData(arg));
+			getMemoryInterface().setData(getMachine().getMemory().getData(arg), retVal);
 		}
 		else{
-			getMemoryInterface().setData(retVal, arg);
+			getMemoryInterface().setData(arg, retVal);
 		}
 		getMachine().incrementCounter();
 		return retVal;
