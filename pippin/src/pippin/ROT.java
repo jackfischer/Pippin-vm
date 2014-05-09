9525ec65dd991ccb9baa76c8baf03d946c1d2be9
sbravo1 <sbravo1@binghamton.edu>
2014-05-09 11:34:06 -0400

    javadoc complete! :D

--- pippin/src/pippin/ROT.java
+++ pippin/src/pippin/ROT.java
@@ -22,12 +22,31 @@ public class ROT extends Instruction {
 	 * - numElems: how many memory addresses will be rotated (this defines the length of the group).
 	 * - numMoves: how many rotations will be performed. If positive,
 	 * @param arg a memory address pointing to where the 3 parameters used in the ROT instruction are stored.
-	 * If
+	 * @param indirect a boolean which, if true, means that arg is a memory address to another address that points to the 3 parameters.
+	 * @return retVal as with other instructions, ROT returns the accumulator.
 	 * 
-	 * @throws IllegalArgumentException 
+	 * There are various conditions that the method deals with:
+	 * @throws IllegalArgumentException If the number of elements to rotate is negative, or if set to be rotated includes the parameters.
+	 * If numElems is 0 or 1, or if the number of moves is 0, then these are trivial conditions, and the process is skipped.
+	 * If numMoves is greater than numElems, then we can increase efficiency by making numMoves = numMoves % numElems.
 	 * 
+	 * The method has an inner and outer loop.
+	 * The inner loop shifts all the elements in the set up/down once, and the outer loop repeats this process numMoves times.
+	 * However, the inner loop requires that one of the elements be copied in a temporary memory location, and then placed in its correct position.
+	 * Here's an example:
+	 * 		assume we are rotating {1,2,3,4} -1 times. The inner loop takes (start+j+1) and stores it in (start+j).
+	 * 		We move {2,2,3,4}, {2,3,3,4}, {2,3,4,4}. However, {1} has been lost. So {1} is stored at another memory location temporarily.
+	 * 		So, for negative rotations, the first element is stored temporarily and lastly placed at the end of the set.
+	 * This process is also necessary for positive rotations:
+	 * 		assume we are rotating {1,2,3,4} +1 times. The inner loop starts from the end of the set, else we would end up with this error: {1,1,1,1}
+	 * 		It takes (start+numElems-1)-j-1 and stores it in (start+numElems-1)-j.
+	 * 		We move {1,2,3,3}, {1,2,2,3}, {1,1,2,3}. Since {4} has been lost, it has to be stored in the temporary memory location.
+	 * 		For positive rotations, the last element is stored temporarily and lastly placed at the start of the set.
 	 * 
-	 *
+	 * ROT calls the method findAnEmptySpace() before starting any rotation process.
+	 * FindAnEmptySpace() will return the first NO-OP location in memory.
+	 * This memory location is used as the temp that was necessary above throughout the operation.
+	 * When the rotation process is finished, this memory location is changed back into a NO-OP.
 	 * 
 	 */
 	@Override 
